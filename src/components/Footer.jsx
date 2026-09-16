c57abd579517c00c359a1605d3c3007bca115df3
Manav0502 <manavsurendraagarwal@gmail.com>
2026-09-16 12:44:30 +0530

    Footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2026 Shoe Store. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
