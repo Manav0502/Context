26bc69ecf6b39518c495618beab64c7cfa87ca49
Manav0502 <manavsurendraagarwal@gmail.com>
2026-09-16 12:40:08 +0530

    Products data added

---
+++ src/data/products.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    title: "U.S. POLO ASSN. Mens PanalSneaker",
+    price: 99,
+    image: "https://images.unsplash.com/photo-1549298916-b41d501d3772?w=500&q=80"
+  },
+  {
+    id: 2,
+    title: "ASIAN Men's Sports Shoes",
+    price: 68,
+    image: "https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?w=500&q=80"
+  },
+  {
+    id: 3,
+    title: "White Casual Sneaker",
+    price: 70,
+    image: "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?w=500&q=80"
+  },
+  {
+    id: 4,
+    title: "MACTREE Men's Mid Top Ankle Boots",
+    price: 90,
+    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500&q=80"
+  },
+  {
+    id: 5,
+    title: "Campus Men's Sneakers",
+    price: 50,
+    image: "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?w=500&q=80"
+  },
+  {
+    id: 6,
+    title: "Campus Men's OG-03 Sneakers",
+    price: 75,
+    image: "https://images.unsplash.com/photo-1608231387042-66d1773070a5?w=500&q=80"
+  }
+];
